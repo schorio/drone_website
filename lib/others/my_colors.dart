461b7252fd8de0b2424afe19cba499926a6e2b1b
schorio <schorioignace@gmail.com>
2024-05-22 17:23:00 +0300

    creation du biblio des couleurs

---
+++ lib/others/my_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static Color white = const Color(0xFFFFFFFF);
+  static Color blue = const Color(0xFF9BD3FC);
+  static Color orange = const Color(0xFFF6E0AE);
+  static Color pink = const Color(0xFFF19FBA);
+  static Color purple = const Color(0xFFC407ED);
+  static Color lightBlue = const Color(0xFF25D3E9);
+  static Color lightPink = const Color(0xFFF9E9FC);
+  static Color darkPurple = const Color(0xFF351E39);
+}
