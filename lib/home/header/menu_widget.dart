c290bde4c5f2c2bee05bb96bdc4d9839a73d87ad
schorio <schorioignace@gmail.com>
2024-05-22 20:06:07 +0300

    MenuWidget : exportation du menu de header en widet

---
+++ lib/home/header/menu_widget.dart
@@ -0,0 +1,68 @@
+import 'package:drone_website/others/my_colors.dart';
+import 'package:flutter/material.dart';
+
+class MenuWidget extends StatelessWidget {
+  const MenuWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 200),
+      child: Row(
+        children: [
+          Text(
+            'Services',
+            style: TextStyle(
+              color: MyColors.white,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(width: 50),
+          Row(
+            children: [
+              Text(
+                'Pricing',
+                style: TextStyle(
+                  color: MyColors.white,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(width: 8),
+              Icon(
+                Icons.keyboard_arrow_down_rounded,
+                size: 25,
+                color: MyColors.white,
+              )
+            ],
+          ),
+          const SizedBox(width: 50),
+          Text(
+            'Drones',
+            style: TextStyle(
+              color: MyColors.white,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(width: 50),
+          Text(
+            'Contact us',
+            style: TextStyle(
+              color: MyColors.white,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(width: 50),
+          Text(
+            'About us',
+            style: TextStyle(
+              color: MyColors.white,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
