4f602ef097d58afb6b4a095efa1f8c6a9e7ad565
schorio <schorioignace@gmail.com>
2024-05-22 16:49:04 +0300

    Premier commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:drone_website/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Drone Website',
+      home: Homepage(),
+    );
+  }
+}
